bbef479c1b1f5d2b839ac5884593079d673ec3bf
sen.lyu <sen.lyu@uconn.edu>
2018-09-26 16:04:23 -0700

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ function start(route, handle){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request " + pathname + " received.");
 
-		request.setEncoding("uft8");
+		request.setEncoding("utf8");
 
 		request.addListener("data", function(postDataChunk){
 			postData += postDataChunk;
