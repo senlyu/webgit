9a01529cec1d36487888e6adb55aa7a486ca22d8
sen.lyu <sen.lyu@uconn.edu>
2018-09-26 15:32:03 -0700

    test5

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,17 @@
 function start(){
 	console.log("Request handler 'start' was called.");
+	exec("ls -lah", function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    });
 }
 
 function upload(){
 	console.log("Request handler 'upload' was called.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -4,6 +4,9 @@ function route(handle, pathname) {
 		handle[pathname]();
 	} else {
 		console.log("No request handler found for " + pathname);
+		response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not found");
+        response.end();
 	}
 }
 
