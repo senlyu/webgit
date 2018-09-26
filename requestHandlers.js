7e30a3e4b942ec1d9fc0f2e6c4fbc82dae91be33
sen.lyu <sen.lyu@uconn.edu>
2018-09-26 15:43:05 -0700

    update

--- requestHandlers.js
+++ requestHandlers.js
@@ -14,7 +14,7 @@ function start(response){
     '</form>'+
     '</body>'+
     '</html>';
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
 }
