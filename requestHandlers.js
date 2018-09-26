a9946adcbf2bcdea0433b3bc45af35628226d2ed
sen.lyu <sen.lyu@uconn.edu>
2018-09-26 16:02:50 -0700

    test7

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response){
+
+function start(response, postData){
 	console.log("Request handler 'start' was called.");
 	var body = '<html>'+
     '<head>'+
@@ -19,10 +21,10 @@ function start(response){
     response.end();
 }
 
-function upload(response){
+function upload(response, postData){
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("Hello Upload:" + querystring.parse(postData).text);
     response.end();
 }
 
